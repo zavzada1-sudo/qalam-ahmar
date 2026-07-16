62ec00de3d084c84597b8b2ca6a70d7dc50eded0
Youssef Mokhtar <zavzada1@gmail.com>
2026-07-17 01:43:27 +0300

    Redesign auth pages + update service worker

--- service-worker.js
+++ service-worker.js
@@ -29,4 +29,15 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((names) =>
       Promise.all(
-        names.filter((name) => name !==
+        names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
